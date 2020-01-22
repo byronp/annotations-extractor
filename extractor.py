f183cecf4e229c892a881a6979f9c953bcb32fa6
Flurin Coretti <hello@flurincoretti.com>
2020-01-22 22:18:28 +0100

    Change SQL queries and print highlights with metadata

--- extractor.py
+++ extractor.py
@@ -21,37 +21,59 @@ def close(con):
     con.close()
 
 
-def get_highlights(cur_notes, cur_books):
-    query = 'SELECT * FROM ZAEANNOTATION WHERE ZANNOTATIONSELECTEDTEXT IS NOT NULL'
-    cur_notes.execute(query)
-    notes = cur_notes.fetchall()
+def get_highlights(cur_annotations, cur_books):
+    query = 'SELECT\
+        Z_PK as id,\
+        ZANNOTATIONSELECTEDTEXT as text,\
+        ZANNOTATIONASSETID as book_id,\
+        ZANNOTATIONSTYLE as style,\
+        datetime(ZANNOTATIONMODIFICATIONDATE+978307200, "unixepoch") as last_modified\
+        FROM\
+        ZAEANNOTATION\
+        WHERE\
+        ZANNOTATIONSELECTEDTEXT IS NOT NULL'
+    cur_annotations.execute(query)
+    annotations = cur_annotations.fetchall()
     highlights = {}
-    for note in notes:
+    for annotation in annotations:
         query = 'SELECT * FROM ZBKLIBRARYASSET WHERE ZASSETID={}'.format(
-            note['ZANNOTATIONASSETID']
+            annotation['book_id']
         )
         cur_books.execute(query)
         book = cur_books.fetchone()
-        highlights[note['Z_PK']] = {
-            'book_id': note['ZANNOTATIONASSETID'],
+        highlights[annotation['id']] = {
+            'book_id': annotation['book_id'],
             'title': book['ZTITLE'],
             'author': book['ZAUTHOR'],
-            'text': note['ZANNOTATIONSELECTEDTEXT']
+            'text': annotation['text'],
+            'last_modified': annotation['last_modified'],
+            'style': annotation['style']
         }
     return highlights
 
 
-def get_highlights(cur_notes, cur_books, book_id):
-    query = 'SELECT * FROM ZAEANNOTATION WHERE \
-        ZANNOTATIONSELECTEDTEXT IS NOT NULL AND \
+def get_highlights(cur_annotations, cur_books, book_id):
+    query = 'SELECT\
+        Z_PK as id,\
+        ZANNOTATIONSELECTEDTEXT as text,\
+        ZANNOTATIONASSETID as book_id,\
+        ZANNOTATIONSTYLE as style,\
+        datetime(ZANNOTATIONMODIFICATIONDATE+978307200, "unixepoch") as last_modified\
+        FROM\
+        ZAEANNOTATION\
+        WHERE\
+        ZANNOTATIONSELECTEDTEXT IS NOT NULL\
+        AND\
         ZANNOTATIONASSETID={}'.format(book_id)
-    cur_notes.execute(query)
-    notes = cur_notes.fetchall()
+    cur_annotations.execute(query)
+    annotations = cur_annotations.fetchall()
     highlights = {}
-    for note in notes:
-        highlights[note['Z_PK']] = {
-            'book_id': note['ZANNOTATIONASSETID'],
-            'text': note['ZANNOTATIONSELECTEDTEXT']
+    for annotation in annotations:
+        highlights[annotation['id']] = {
+            'book_id': annotation['book_id'],
+            'text': annotation['text'],
+            'last_modified': annotation['last_modified'],
+            'style': annotation['style']
         }
     return highlights
 
@@ -81,27 +103,37 @@ def get_books(cur):
 
 def main():
     path = 'Library/Containers/com.apple.iBooksX/Data/Documents'
-    notes_db_name = 'AEAnnotation_v10312011_1727_local.sqlite'
+    annotations_db_name = 'AEAnnotation_v10312011_1727_local.sqlite'
     books_db_name = 'BKLibrary-1-091020131601.sqlite'
-    notes_db = os.path.join(HOME, path, 'AEAnnotation', notes_db_name)
+    annotations_db = os.path.join(HOME, path, 'AEAnnotation', annotations_db_name)
     books_db = os.path.join(HOME, path, 'BKLibrary', books_db_name)
     temp_dir = tempfile.gettempdir()
-    temp_notes_db = os.path.join(temp_dir, 'temp_notes_database.sqlite')
+    temp_annotations_db = os.path.join(temp_dir, 'temp_annotations_database.sqlite')
     temp_books_db = os.path.join(temp_dir, 'temp_books_database.sqlite')
-    shutil.copyfile(notes_db, temp_notes_db) # Achtung!
+    shutil.copyfile(annotations_db, temp_annotations_db) # Achtung!
     shutil.copyfile(books_db, temp_books_db) # Achtung!
-    con_notes, cur_notes = connect(temp_notes_db)
+    con_annotations, cur_annotations = connect(temp_annotations_db)
     con_books, cur_books = connect(temp_books_db)
-    # Print highlights grouped by book
+    # Print highlights with metadata
+    style = {0:'Underline', 1:'Green', 2:'Blue', 3:'Yellow', 4:'Pink', 5:'Purple'}
     books = get_books(cur_books)
     book_ids = [k for k, v in books.items()]
     for book_id in book_ids:
-        highlights = get_highlights(cur_notes, cur_books, book_id)
+        highlights = get_highlights(cur_annotations, cur_books, book_id)
         if highlights != {}:
-            print('# {}\n'.format(books[book_id]['title']))
-            for highlight in [v['text'] for k, v in highlights.items()]:
-                print('{}\n\n---\n\n'.format(highlight))
-    close(con_notes)
+            for k in highlights.keys():
+                print('> {}\
+                \n\n{}, _{}_\
+                \n\nLast modified: {}\
+                \n\nHighlight color: {}\
+                \n\n---\n'.format(
+                    highlights[k]['text'],
+                    books[book_id]['author'],
+                    books[book_id]['title'],
+                    highlights[k]['last_modified'],
+                    style[highlights[k]['style']]
+                ))
+    close(con_annotations)
     close(con_books)
 
 
