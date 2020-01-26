ccf1b3e795d795d493ef576ab5d149c513866835
Flurin Coretti <hello@flurincoretti.com>
2020-01-26 21:43:04 +0100

    Refactor the script

--- extractor.py
+++ extractor.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 
 HOME = os.getenv('HOME', '')
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
 def connect(db):
@@ -16,35 +17,7 @@ def connect(db):
     return con, cur
 
 
-def close(con):
-    # Closes a database connection.
-    con.close()
-
-
-def get_highlights(cur_annotations, cur_books):
-    query = ('SELECT Z_PK as id, ZANNOTATIONSELECTEDTEXT as text, '
-             'ZANNOTATIONASSETID as book_id, ZANNOTATIONSTYLE as style, '
-             'datetime(ZANNOTATIONMODIFICATIONDATE + 978307200, "unixepoch", "localtime") as last_modified '
-             'FROM ZAEANNOTATION '
-             'WHERE ZANNOTATIONSELECTEDTEXT IS NOT NULL '
-             'ORDER BY last_modified ASC')
-    cur_annotations.execute(query)
-    annotations = cur_annotations.fetchall()
-    highlights = {}
-    for annotation in annotations:
-        book = get_book(cur_books, annotation['book_id'])
-        highlights[annotation['id']] = {
-            'book_id': annotation['book_id'],
-            'title': book['title'],
-            'author': book['author'],
-            'text': annotation['text'],
-            'last_modified': annotation['last_modified'],
-            'style': annotation['style']
-        }
-    return highlights
-
-
-def get_highlights_2(cur):
+def get_highlights(cur):
     query = ('SELECT Z_PK as id, ZANNOTATIONSELECTEDTEXT as text, '
              'ZANNOTATIONASSETID as book_id, ZANNOTATIONSTYLE as style, '
              'ZANNOTATIONCREATIONDATE as created, '
@@ -69,7 +42,7 @@ def get_book(cur, id):
     return cur.fetchone()
 
 
-def dump_db():
+def initialize():
     temp_dir = tempfile.gettempdir()
     path = 'Library/Containers/com.apple.iBooksX/Data/Documents'
 
@@ -87,12 +60,15 @@ def dump_db():
     shutil.copyfile(b_db, b_db_temp) # Achtung!
     con_b, cur_b = connect(b_db_temp)
 
-    # Create an output database
+    # Create an output database (delete existing one)
     output_db_name = 'annotations.sqlite'
-    output_db = os.path.join(HOME, 'Desktop', output_db_name)
+    output_db = os.path.join(PROJECT_ROOT, 'data', output_db_name)
+    print(output_db)
+    if os.path.isfile(output_db):
+        os.remove(output_db)
     con_o = sqlite3.connect(output_db)
     cur_o = con_o.cursor()
-    query = ('CREATE TABLE highlights ('
+    query = ('CREATE TABLE IF NOT EXISTS highlights ('
             'id INTEGER PRIMARY KEY AUTOINCREMENT, '
             'book_id VARCHAR, '
             'title VARCHAR, '
@@ -104,7 +80,7 @@ def dump_db():
     cur_o.execute(query)
 
     # Insert data into the output database
-    highlights = get_highlights_2(cur_a)
+    highlights = get_highlights(cur_a)
     for highlight in highlights:
         book = get_book(cur_b, highlight['book_id'])
         query = ('INSERT INTO highlights '
@@ -123,33 +99,31 @@ def dump_db():
     
 
 def main():
-    path = 'Library/Containers/com.apple.iBooksX/Data/Documents'
-    annotations_db_name = 'AEAnnotation_v10312011_1727_local.sqlite'
-    books_db_name = 'BKLibrary-1-091020131601.sqlite'
-    annotations_db = os.path.join(HOME, path, 'AEAnnotation', annotations_db_name)
-    books_db = os.path.join(HOME, path, 'BKLibrary', books_db_name)
-    temp_dir = tempfile.gettempdir()
-    temp_annotations_db = os.path.join(temp_dir, 'temp_annotations_database.sqlite')
-    temp_books_db = os.path.join(temp_dir, 'temp_books_database.sqlite')
-    shutil.copyfile(annotations_db, temp_annotations_db) # Achtung!
-    shutil.copyfile(books_db, temp_books_db) # Achtung!
-    con_annotations, cur_annotations = connect(temp_annotations_db)
-    con_books, cur_books = connect(temp_books_db)
-    # Print highlights with metadata
+    initialize()
+    annotations_db_name = 'annotations.sqlite'
+    annotations_db = os.path.join(PROJECT_ROOT, 'data', annotations_db_name)
+    con, cur = connect(annotations_db)
+    # Print all highlights with metadata
+    query = ('SELECT title, author, text, style, '
+             'datetime(created + 978307200, "unixepoch", "localtime") as created, '
+             'datetime(last_modified + 978307200, "unixepoch", "localtime") as last_modified '
+             'FROM highlights '
+             'ORDER BY created')
+    cur.execute(query)
+    highlights = cur.fetchall()
     style = {0:'Underline', 1:'Green', 2:'Blue', 3:'Yellow', 4:'Pink', 5:'Purple'}
-    highlights = get_highlights(cur_annotations, cur_books)
-    for id in highlights: 
-        highlight = highlights[id] 
+    for highlight in highlights: 
         print('> {}\n'.format(highlight['text'])) 
-        print('{}, _{}_'.format(highlight['author'], highlight['title'])) 
-        print('Highlight last modified on {}'.format(highlight['last_modified'])) 
+        print('{}, _{}_.'.format(highlight['author'], highlight['title']))
+        print('\nMetadata:')
+        print('* Created: {}'.format(highlight['created'])) 
+        print('* Last modified: {}'.format(highlight['last_modified'])) 
         if highlight['style'] == 0: 
-            print('Underlined')  
+            print('* Style: Underlined')  
         else: 
-            print('Highlighted in {}'.format(style[highlight['style']].lower())) 
+            print('* Style: {} highlight'.format(style[highlight['style']]))
         print('\n---\n')
-    close(con_annotations)
-    close(con_books)
+    con.close()
 
 
 if __name__ == '__main__':
